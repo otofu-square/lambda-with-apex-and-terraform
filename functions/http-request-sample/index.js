458c1d7d48183be80a972d160ec1c9bb46d8168f
otofu-square <yukita@feedforce.jp>
2016-10-25 18:04:09 +0900

    Send HTTP GET request

--- functions/http-request-sample/index.js
+++ functions/http-request-sample/index.js
@@ -1,7 +1,16 @@
-
+const axios = require("axios");
 
 console.log('starting function')
 exports.handle = function(e, ctx, cb) {
+  axios.get("https://www.google.co.jp")
+    .then((response) => {
+      console.log(response);
+      cb(null, response.status);
+    })
+    .catch((error) => {
+      console.log(error);
+      cb(null, error.status);
+    });
+
   console.log('processing event: %j', e)
-  cb(null, { hello: 'world' })
 }
